4ee51330e1b2747cd7fbe9ed38e15b4dea12a157
Arsenkzn <archi1504@gmail.com>
2025-06-17 15:55:32 +0300

    fix: persist и devTools сделал

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,31 +1,66 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import { thunk, ThunkAction, ThunkMiddleware } from "redux-thunk";
+import {
+  createStore,
+  applyMiddleware,
+  combineReducers,
+  Reducer,
+  Store,
+} from "redux";
+import {
+  thunk,
+  ThunkAction,
+  ThunkDispatch,
+  ThunkMiddleware,
+} from "redux-thunk";
 import { contactsReducer } from "./reducers/contacts";
 import { groupContactsReducer } from "./reducers/group-reducer";
 import { ProjectActions } from "./actions/actions";
 import { logAtionMiddleware } from "src/pages/logAtionMiddleware";
 import { ContactsState, GroupsState } from "./types";
+import { persistStore } from "redux-persist";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistPartial } from "redux-persist/es/persistReducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-interface RootState {
+export interface RootState {
   contacts: ContactsState;
   groups: GroupsState;
 }
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  groups: groupContactsReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const typedContactsReducer: Reducer<ContactsState, ProjectActions> =
+  contactsReducer;
+const typedGroupsReducer: Reducer<GroupsState, ProjectActions> =
+  groupContactsReducer;
+
+const rootReducer: Reducer<RootState, ProjectActions> = combineReducers({
+  contacts: typedContactsReducer,
+  groups: typedGroupsReducer,
 });
 
-const initialState = {};
+const persistedReducer = persistReducer<RootState, ProjectActions>(
+  persistConfig,
+  rootReducer
+);
 
-export const store = createStore(
-  rootReducer,
-  initialState,
-  applyMiddleware(
-    thunk as ThunkMiddleware<RootState, ProjectActions>,
-    logAtionMiddleware
+export const store: Store<RootState & PersistPartial, ProjectActions> & {
+  dispatch: ThunkDispatch<RootState, undefined, ProjectActions>;
+} = createStore(
+  persistedReducer,
+  undefined,
+  composeWithDevTools(
+    applyMiddleware(
+      thunk as ThunkMiddleware<RootState, ProjectActions>,
+      logAtionMiddleware
+    )
   )
 );
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk = ThunkAction<void, RootState, unknown, ProjectActions>;
